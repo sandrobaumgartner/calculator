7e20d57f980be2402742e9072cc9cf3f8d8e7804
sandrobaumgartner <s50678@edu.campus02.at>
2021-10-08 19:58:44 +0200

    added variables

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,16 @@
 public class Calculator {
 
     public static void main(String[] args) {
+        int a = 1;
+        double b = 2.3;
+        boolean c = false;
+        char d = 'A';
+        String e = "test";
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
