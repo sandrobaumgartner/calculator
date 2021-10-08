e991261212c1e16439223390e4f6426c5827b1c9
sandrobaumgartner <s50678@edu.campus02.at>
2021-10-08 21:29:41 +0200

    added methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,16 +1,37 @@
 public class Calculator {
 
     public static void main(String[] args) {
-        int a = 1;
-        double b = 2.3;
-        boolean c = false;
-        char d = 'A';
-        String e = "test";
+        int a = 10;
+        int b = 16;
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
+        printResult(add(a,b));
+        printResult(sub(a,b));
+        printResult(div(a,b));
+        printResult(mul(a,b));
+        printResult(mod(a,b));
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static int sub(int a, int b){
+        return a-b;
+    }
+
+    public static int div(int a, int b){
+        return a/b;
+    }
+
+    public static int mul(int a, int b){
+        return a*b;
+    }
+
+    public static int mod(int a, int b){
+        return a%b;
+    }
+
+    public static void printResult(int result){
+        System.out.println(result);
     }
 }
